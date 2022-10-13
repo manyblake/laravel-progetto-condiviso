9623dad9f6df1934daed15096e9f1426962b6090
Francesco Pieraccini <pieraccini.francesco@gmail.com>
2022-10-13 17:29:07 +0200

    add departments layout

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Department;
+
 use Illuminate\Http\Request;
 
 class DepartmentController extends Controller
 {
     public function show()
     {
-        return view('department.index');
+        $departments = Department::all();
+        $data = compact('departments');
+        return view('department.index', $data);
     }
 }
--- resources/views/department/index.blade.php
+++ resources/views/department/index.blade.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Departments</title>
 </head>
 <body>
     <h1>Departments</h1>
+
+    <div>
+        @forelse ($departments as $department)
+        <p>
+            {{ $department->name }}
+        </p>
+            
+        @empty
+            <p>
+                Nessun dipartimento trovato.
+            </p>
+        @endforelse
+    </div>
 </body>
 </html>
